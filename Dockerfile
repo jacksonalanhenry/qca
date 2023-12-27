2344773e82ca7da044e4f717d4bea81484ad157b
jacksonalanhenry <jhenry11094@gmail.com>
2023-12-27 16:37:24 -0700

    docker and runcmd things

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
  FROM ubuntu:22.04
  RUN ln -snf /usr/share/zoneinfo/$CONTAINER_TIMEZONE /etc/localtime && echo $CONTAINER_TIMEZONE > /etc/timezone
- RUN apt-get update && apt-get install -y python3-tk python3-pip
+ RUN apt-get update && apt-get install -y python3-tk python3-pip libx11-dev
  RUN pip3 install matplotlib
  COPY ./app /app
  CMD /bin/bash
--- runcmd.sh
+++ runcmd.sh
@@ -1,2 +1,4 @@
-#docker run --rm -it --env=DISPLAY qca:latest python3 /app/main.py
+#docker run --rm -it --env=DISPLAY -v ~/qca/:/qca/ qca:latest python3 /qca/app/main.py
 docker run --rm -it -e DISPLAY=host.docker.internal:0 -v ~/qca/:/qca/ qca:latest python3 /qca/app/main.py
+#docker run --rm -it -e DISPLAY=$DISPLAY -v ~/qca/:/qca/ qca:latest python3 /qca/app/main.py
+#viu ~/qca/fig.png
